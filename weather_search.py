7ab10b8dbc28d188528162117b7557be056e7d6e
Daniel Bak <DanielPBak@gmail.com>
2018-04-09 03:56:11 -0600

    Fixed a bug causing days above 50 km/h to be incorrect.

--- weather_search.py
+++ weather_search.py
@@ -26,7 +26,7 @@ try:
     days_above_50 = data['Spd of Max Gust (km/h)']
     # Assumption: all values are either integers or "<31"
     days_above_50 = pandas.to_numeric(days_above_50, errors='coerce')
-    days_above_50 = days_above_50 >= 50.0
+    days_above_50 = days_above_50.loc[days_above_50 >= 50.0]
     days_above_50 = days_above_50.shape[0]
     ret['days_above_50'] = str(days_above_50)
     print(json.dumps(ret))
